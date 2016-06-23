78e031334530dbf2a861014a5d2a77bf24a015c1
ASteinheiser <asteinhe@asu.edu>
2016-06-22 22:45:24 -0700

    layout is finally solid

--- src/containers/home.js
+++ src/containers/home.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Description from '../components/Description/index'
 
+import styles from './styles.css'
+
 export default class Home extends React.Component {
   render() {
     return (
-      <div>
-        <Description />
+      <div className={[styles.contentFrame]}>
+        <div className={[styles.hiddenScrollbar]}>
+          <Description />
+        </div>
       </div>
     )
   }
