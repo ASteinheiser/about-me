7e8716170f3c28b49b60fa6240e7392f3bbb7901
ASteinheiser <asteinhe@asu.edu>
2016-08-09 21:15:08 -0700

    getting ready to go online

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+const port = process.env.port || 80
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public/dist')))
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res) {
+  res.status(err.status || 500)
+  res.render('error', {
+    message: err.message,
+    error: {},
+  })
+})
+
+app.listen(port, function () {
+  console.log("Server started on port: ", port)
+})
