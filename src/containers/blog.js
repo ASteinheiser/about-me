4997fc518fd82420783fbdcbc5c79f717ce8ebe6
ASteinheiser <asteinhe@asu.edu>
2017-01-01 18:24:08 -0700

    top bar fixed position, updated blog section

--- src/containers/blog.js
+++ src/containers/blog.js
@@ -10,21 +10,21 @@ export default class Blog extends React.Component {
     return (
       <div className={[styles.innerWidth]}>
         <br />
+
         <StyledText
-          text="Discuss anything tech related..."
+          text="Recent projects, new tech, etc."
           size="large"
           color="blue"
         />
+
         <br />
         <br />
+
         <BlogPost
-          title="Hello World"
-          body="This is a hello world blog post... Hello world... This is a hello world blog post... Hello world... This is a hello world blog post... Hello world..."
+          title="Example Post"
+          body="Spicy jalapeno bacon ipsum dolor amet pork dolore t-bone ea occaecat pariatur id ex cupim ut kevin sunt. Eiusmod t-bone ex flank shankle tenderloin. Beef cillum eu landjaeger. Tail meatball pastrami andouille do. Proident duis spare ribs enim magna prosciutto ea kevin pig strip steak. Kevin spare ribs alcatra labore eu. Ex picanha ipsum, turducken pork chop quis laboris. Do quis pancetta nostrud officia voluptate. Enim pancetta culpa consequat ham dolore ea. Tri-tip duis do sed ipsum jerky picanha. Consequat ex cupidatat, andouille aute t-bone sunt ipsum porchetta capicola adipisicing aliqua id. Pork chop velit nostrud enim est laborum bacon minim."
         />
-        <BlogPost
-          title="Hello World"
-          body="This is a hello world blog post... Hello world..."
-        />
+
         <BlogPost
           title="Hello World"
           body="This is a hello world blog post... Hello world..."
