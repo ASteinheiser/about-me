d623eda9df544e1aa970964c29ab29cd15c51258
ASteinheiser <asteinhe@asu.edu>
2017-01-02 23:22:10 -0700

    blog coming soon...

--- src/containers/blog.js
+++ src/containers/blog.js
@@ -12,7 +12,7 @@ export default class Blog extends React.Component {
         <br />
 
         <StyledText
-          text="Recent projects, new tech, etc."
+          text="Blog"
           size="large"
           color="blue"
         />
@@ -21,14 +21,10 @@ export default class Blog extends React.Component {
         <br />
 
         <BlogPost
-          title="Example Post"
-          body="Spicy jalapeno bacon ipsum dolor amet pork dolore t-bone ea occaecat pariatur id ex cupim ut kevin sunt. Eiusmod t-bone ex flank shankle tenderloin. Beef cillum eu landjaeger. Tail meatball pastrami andouille do. Proident duis spare ribs enim magna prosciutto ea kevin pig strip steak. Kevin spare ribs alcatra labore eu. Ex picanha ipsum, turducken pork chop quis laboris. Do quis pancetta nostrud officia voluptate. Enim pancetta culpa consequat ham dolore ea. Tri-tip duis do sed ipsum jerky picanha. Consequat ex cupidatat, andouille aute t-bone sunt ipsum porchetta capicola adipisicing aliqua id. Pork chop velit nostrud enim est laborum bacon minim."
+          title="Coming Soon..."
+          body="Recent projects, new tech, etc."
         />
 
-        <BlogPost
-          title="Hello World"
-          body="This is a hello world blog post... Hello world..."
-        />
       </div>
     )
   }
