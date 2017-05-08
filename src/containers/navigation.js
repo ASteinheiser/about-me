2800f050e26520e4ed81266ba6203464935f299f
ASteinheiser <asteinhe@asu.edu>
2017-05-08 09:08:34 -0700

    potential background images

--- src/containers/navigation.js
+++ src/containers/navigation.js
@@ -7,7 +7,13 @@ import styles from './styles.css'
 const propTypes = {
   children: PropTypes.element.isRequired,
 }
-
+// https://static.pexels.com/photos/160107/pexels-photo-160107.jpeg
+// https://images.pexels.com/photos/79290/black-and-white-code-programming-tech-79290.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb
+// https://images.pexels.com/photos/92904/pexels-photo-92904.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb
+// https://images.pexels.com/photos/214221/pexels-photo-214221.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb
+// https://images.pexels.com/photos/48606/pexels-photo-48606.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb
+// https://images.pexels.com/photos/34578/pexels-photo.jpg?w=1260&h=750&auto=compress&cs=tinysrgb
+// https://images.pexels.com/photos/34580/pexels-photo.jpg?w=1260&h=750&auto=compress&cs=tinysrgb
 export default class Navigation extends React.Component {
   render() {
     return (
