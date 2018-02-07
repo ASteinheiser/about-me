dbbef40558b4c9a0278e0978237e03b883e32da8
Andrew Steinheiser <me@iamandrew.io>
2018-02-06 23:09:23 -0700

    final tweaks to contact page

--- src/components/social-links/index.js
+++ src/components/social-links/index.js
@@ -9,68 +9,69 @@ export default class SocialLinks extends React.Component {
 
   render() {
     return (
-      <div className={[styles.center]}>
-        <a target="_blank" href="https://github.com/ASteinheiser" className={[styles.socialButton]}>
-          <i className="fa fa-github-square" />
-          <div className={[styles.divider]} />
-          <StyledText
-            text="Github"
-            size="small"
-            topAlign
-          />
-        </a>
+      <div>
+        <div className={[styles.center]}>
+          <a target="_blank" href="https://github.com/ASteinheiser" className={[styles.socialButton]}>
+            <i className="fa fa-github-square" />
+            <div className={[styles.divider]} />
+            <StyledText
+              text="Github"
+              size="small"
+              topAlign
+            />
+          </a>
 
-        <a target="_blank" href="https://www.hackster.io/andrewstein" className={[styles.socialButton]}>
-          <img className={[styles.hackster]} src={hacksterLogo} role="presentation" />
-          <StyledText
-            text="Hackster"
-            size="small"
-            topAlign
-          />
-        </a>
+          <a target="_blank" href="https://www.hackster.io/andrewstein" className={[styles.socialButton]}>
+            <img className={[styles.hackster]} src={hacksterLogo} role="presentation" />
+            <StyledText
+              text="Hackster"
+              size="small"
+              topAlign
+            />
+          </a>
 
-        <a target="_blank" href="https://www.instagram.com/asteinheiser/" className={[styles.socialButton]}>
-          <i className="fa fa-instagram" />
-          <div className={[styles.divider]} />
-          <StyledText
-            text="Instagram"
-            size="small"
-            topAlign
-          />
-        </a>
+          <a target="_blank" href="https://www.instagram.com/asteinheiser/" className={[styles.socialButton]}>
+            <i className="fa fa-instagram" />
+            <div className={[styles.divider]} />
+            <StyledText
+              text="Instagram"
+              size="small"
+              topAlign
+            />
+          </a>
+        </div>
 
-        <br/>
-        <br/>
+        <div className={[styles.center]}>
+          <a target="_blank" href="https://www.linkedin.com/in/andrew-steinheiser-8989b1122" className={[styles.socialButton]}>
+            <i className="fa fa-linkedin" />
+            <div className={[styles.divider]} />
+            <StyledText
+              text="Linkedin"
+              size="small"
+              topAlign
+            />
+          </a>
 
-        <a target="_blank" href="https://www.linkedin.com/in/andrew-steinheiser-8989b1122" className={[styles.socialButton]}>
-          <i className="fa fa-linkedin" />
-          <div className={[styles.divider]} />
-          <StyledText
-            text="Linkedin"
-            size="small"
-            topAlign
-          />
-        </a>
+          <a target="_blank" href="https://twitter.com/ASteinheiser" className={[styles.socialButton]}>
+            <i className="fa fa-twitter-square" />
+            <div className={[styles.divider]} />
+            <StyledText
+              text="Twitter"
+              size="small"
+              topAlign
+            />
+          </a>
 
-        <a target="_blank" href="https://twitter.com/ASteinheiser" className={[styles.socialButton]}>
-          <i className="fa fa-twitter-square" />
-          <div className={[styles.divider]} />
-          <StyledText
-            text="Twitter"
-            size="small"
-            topAlign
-          />
-        </a>
-
-        <a target="_blank" href="https://www.facebook.com/andrewsteinheiser" className={[styles.socialButton]}>
-          <i className="fa fa-facebook" />
-          <div className={[styles.divider]} />
-          <StyledText
-            text="Facebook"
-            size="small"
-            topAlign
-          />
-        </a>
+          <a target="_blank" href="https://www.facebook.com/andrewsteinheiser" className={[styles.socialButton]}>
+            <i className="fa fa-facebook" />
+            <div className={[styles.divider]} />
+            <StyledText
+              text="Facebook"
+              size="small"
+              topAlign
+            />
+          </a>
+        </div>
       </div>
     )
   }
